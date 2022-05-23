e796de296fea87162103022f467cfa4128a7a511
mykealw <mykeal.wheeler@gmail.com>
2022-05-23 16:22:59 -0600

    Rock paper scissors

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿bool valid = false;
+while(valid == false){
+
+Console.WriteLine("Want to play a game..... of Rock, Paper, Scissors?");
+Console.WriteLine("press (1) to play, press (2) if you don't like fun.");
+bool validEntry = false; 
+Console.ReadKey().KeyChar;
+char entry =
+}
